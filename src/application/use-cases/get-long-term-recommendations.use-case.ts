4ed08f610e2340eaf90e695b078d7fa5530c5f64
GYUTORY <azure0563@gmail.com>
2025-08-25 22:40:22 +0900

    feat: 기간별 코인 추천 시스템 use case 추가 (단기/중기/장기)

---
+++ src/application/use-cases/get-long-term-recommendations.use-case.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CoinRecommendationService } from '../../infrastructure/services/coin-recommendation.service';
+import { CoinRecommendationResponseDto } from '../../shared/dto/recommendation.dto';
+
+@Injectable()
+export class GetLongTermRecommendationsUseCase {
+  constructor(
+    private readonly coinRecommendationService: CoinRecommendationService
+  ) {}
+
+  async execute(): Promise<CoinRecommendationResponseDto> {
+    return await this.coinRecommendationService.generateLongTermRecommendations();
+  }
+}
+
+
+
+
