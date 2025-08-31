3e9bcf0488b164dde2e461f59042a42b0febf17a
GYUTORY <azure0563@gmail.com>
2025-08-31 10:45:35 +0900

    feat: Domain Events 수정 - 새로운 폴더 구조에 맞게 import 경로 업데이트

--- src/domain/events/price-updated.event.ts
+++ src/domain/events/price-updated.event.ts
@@ -4,7 +4,7 @@
  * WebSocket으로부터 새로운 가격 데이터가 수신되었을 때 발행되는 이벤트입니다.
  * 이벤트 기반 아키텍처를 통해 느슨한 결합을 구현합니다.
  */
-import { Price } from '../entities/price.entity';
+import { Price } from '../entities/price';
 
 export class PriceUpdatedEvent {
   constructor(
